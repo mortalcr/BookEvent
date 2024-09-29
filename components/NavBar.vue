f22ae2b42bcabb10e015966daf2ed11bae600c99
mortalcr <daporrasn@gmail.com>
2024-09-28 19:13:46 -0600

    fix: Home button

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -34,7 +34,7 @@
     </div>
     <div class="navbar-center hidden lg:flex">
         <ul class="menu menu-horizontal px-1">
-        <li><a>Home</a></li>
+        <li><a href="/">Home</a></li>
         <li><a href="about">About</a></li>
         <li><a href="contact">Contact</a></li>
         </ul>
