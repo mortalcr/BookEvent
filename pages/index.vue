e813a87badf714b07d761828501d9fda56af7b47
mortalcr <daporrasn@gmail.com>
2024-09-28 18:53:53 -0600

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <div class="text-center">
+      <h1 class="text-4xl font-bold mb-4">Welcome to BookEvent</h1>
+      <p class="text-lg mb-6">We're glad to have you here. Explore and Enjoy!</p>
+      <button class="btn">Explore!</button>
+    </div>
+  </div>
+</template>
