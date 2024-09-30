8af9630ba652670a405327e9a784294bcbdb589f
zSnails <erizojuan33@gmail.com>
2024-09-29 22:23:22 -0600

    feat: add `@nuxtjs/color-mode` setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: true },
+    colorMode: {
+        preference: 'cupcake', // default theme
+        dataValue: 'theme', // activate data-theme in <html> tag
+    },
 })
