bd372f8b9a8ae7695a4418840d2ee03a560ca77b
zSnails <erizojuan33@gmail.com>
2024-09-30 23:54:48 -0600

    feat: add supabase dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   plugins: ["~/plugins/v-calendar.js"],
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode",  "@nuxtjs/supabase"],
   colorMode: {
     preference: "cupcake", // default theme
     dataValue: "theme", // activate data-theme in <html> tag
   },
+  supabase: {
+    redirect: false
+  }
 });
