33c534af3648d346257e4ed08b431e415d9b2838
zSnails <erizojuan33@gmail.com>
2024-11-19 20:18:40 -0600

    refactor: translate english text into spanish

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="flex items-center justify-center">
     <div class="text-center">
-      <h1 class="text-4xl font-bold mb-4">Welcome to BookEvent</h1>
-      <p class="text-lg mb-6">We're glad to have you here. Explore and Enjoy!</p>
-      <button class="btn">Explore!</button>
+      <h1 class="text-4xl font-bold mb-4">Bienvenidos al Rancho Fernández</h1>
+      <!-- <p class="text-lg mb-6">We're glad to have you here. Explore and Enjoy!</p> -->
+      <p class="text-lg mb-6">Nos alegra que estés aquí. ¡Explora y disfruta!</p>
+      <button class="btn">Explorar</button>
     </div>
   </div>
 </template>
